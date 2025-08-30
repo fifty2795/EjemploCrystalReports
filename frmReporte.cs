612cda3777189502ff82189ad25cbce1d90d5b31
Alejandro Arizmendi <alejandro_arizmendi@hotmail.es>
2025-08-30 00:13:31 -0600

    Agregar archivos de proyecto.

---
+++ frmReporte.cs
@@ -0,0 +1,116 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+using CrystalReportsExample.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace CrystalReportsExample
+{
+    public partial class frmRptResumenVenta : Form
+    {
+        public frmRptResumenVenta()
+        {
+            InitializeComponent();
+        }
+
+        private void frmRptResumenVenta_Load(object sender, EventArgs e)
+        {
+            var clientes = ObtenerClientes();
+
+            clientes.Insert(0, new Cliente { IdCliente = 0, Nombre = "Todos" });
+
+            cmbClientes.DataSource = clientes;
+            cmbClientes.DisplayMember = "Nombre";   
+            cmbClientes.ValueMember = "IdCliente"; 
+            cmbClientes.SelectedIndex = 0;
+
+            dtpInicio.ShowCheckBox = true;
+            dtpInicio.Checked = false;  
+
+            dtpFin.ShowCheckBox = true;
+            dtpFin.Checked = false;            
+        }
+
+        private void btnGenerarRpt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DateTime? fechaInicio = dtpInicio.Checked ? dtpInicio.Value.Date : (DateTime?)null;
+                DateTime? fechaFin = dtpFin.Checked ? dtpFin.Value.Date : (DateTime?)null;
+                int? clienteId = null;
+
+                var report = new ReportDocument();
+
+                report.Load(@"D:\repos\CrystalReportsExample\CrystalReport1.rpt");
+
+                if (cmbClientes.SelectedValue.ToString() != "0")
+                {
+                    if (int.TryParse(cmbClientes.SelectedValue.ToString(), out var cid)) clienteId = cid;
+                }
+
+                var connInfo = new ConnectionInfo
+                {
+                    ServerName = @"ALEJANDRO\SQLEXPRESS",
+                    DatabaseName = "MVC",
+                    //UserID = "usuario_sql",
+                    //Password = "password_sql",
+                    IntegratedSecurity = true
+                };
+
+                foreach (Table table in report.Database.Tables)
+                {
+                    var logonInfo = table.LogOnInfo;
+                    logonInfo.ConnectionInfo = connInfo;
+                    table.ApplyLogOnInfo(logonInfo);
+
+                    table.Location = table.Location;
+                }
+
+                report.SetParameterValue("@fechaInicio", (object)fechaInicio ?? DBNull.Value);
+                report.SetParameterValue("@fechaFin", (object)fechaFin ?? DBNull.Value);
+                report.SetParameterValue("@idCliente", (object)clienteId ?? DBNull.Value);
+
+                crystalReportViewer1.ReportSource = report;
+                crystalReportViewer1.Refresh();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocurrio un error al generar el reporte." + ex.Message);
+            }
+        }
+
+        private List<Cliente> ObtenerClientes()
+        {
+            var lista = new List<Cliente>();
+
+            using (var cn = new SqlConnection(@"Server=ALEJANDRO\SQLEXPRESS;Database=MVC;Integrated Security=True;"))
+            using (var cmd = new SqlCommand("select Id_Cliente, Nombre from [dbo].[tbl_Cliente]", cn))
+            {
+                cn.Open();
+                using (var dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        lista.Add(new Cliente
+                        {
+                            IdCliente = dr.GetInt32(0),
+                            Nombre = dr.GetString(1)
+                        });
+                    }
+                }
+            }
+
+            return lista;
+        }
+    }
+}
---
+++ mdiMenu.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace CrystalReportsExample
+{
+    partial class mdiMenu
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mdiMenu));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.rEPORTESToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reporteDeResumenDeVentaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.rEPORTESToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(843, 28);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // rEPORTESToolStripMenuItem
+            // 
+            this.rEPORTESToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.reporteDeResumenDeVentaToolStripMenuItem});
+            this.rEPORTESToolStripMenuItem.Name = "rEPORTESToolStripMenuItem";
+            this.rEPORTESToolStripMenuItem.Size = new System.Drawing.Size(91, 24);
+            this.rEPORTESToolStripMenuItem.Text = "REPORTES";
+            // 
+            // reporteDeResumenDeVentaToolStripMenuItem
+            // 
+            this.reporteDeResumenDeVentaToolStripMenuItem.Name = "reporteDeResumenDeVentaToolStripMenuItem";
+            this.reporteDeResumenDeVentaToolStripMenuItem.Size = new System.Drawing.Size(287, 26);
+            this.reporteDeResumenDeVentaToolStripMenuItem.Text = "Reporte de resumen de venta";
+            this.reporteDeResumenDeVentaToolStripMenuItem.Click += new System.EventHandler(this.reporteDeResumenDeVentaToolStripMenuItem_Click);
+            // 
+            // mdiMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(843, 558);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "mdiMenu";
+            this.Text = "Menu";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem rEPORTESToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reporteDeResumenDeVentaToolStripMenuItem;
+    }
+}
+
+
+
